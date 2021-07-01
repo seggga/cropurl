ca36798c718063a8b6117c641b349ffb6645c7b0
Serg Kotovsky <rokulab@mail.ru>
2021-07-02 01:10:18 +0300

    delete unused files

--- internal/cropurl/user/datatype/user.go
+++
@@ -1,8 +0,0 @@
-package datatype
-
-type User struct {
-	UserID   *string `json:"user_id"`
-	UserName *string `json:"user_name"`
-	UserPass *string `json:"user_pass"`
-	UserData *string `json:"user_data"`
-}
